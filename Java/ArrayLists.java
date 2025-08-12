a6c71f31fe06f0990a3d4f28b197d8c658d0a1c1
vinay-021 <vinaysh021@gmail.com>
2025-08-12 10:28:43 +0530

    12082025

--- Java/ArrayLists.java
+++ Java/ArrayLists.java
@@ -1,8 +1,32 @@
 import java.util.*;
-public class ArrayLists{
-    public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
-        ArrayList<Integer> list=new ArrayList<>();
 
+public class ArrayLists {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // Add
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println(list);
+
+        //Add at a specific index
+        list.add(1,40);
+        System.out.println(list);
+
+        //Get a specific element at a particular index
+        System.out.println(list.get(2));
+
+        //Set a element at a particular index
+        list.set(2, 50);
+        System.out.println(list);
+
+        //Remove a element at a particular index
+        list.remove(2);
+        System.out.println(list);
+
+        //Return Size of List
+        System.out.println(list.size());
     }
 }
