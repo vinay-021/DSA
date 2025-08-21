7858882d2700166cf920d2993c5d17eae18a840e
vinay-021 <vinaysh021@gmail.com>
2025-08-21 17:49:18 +0530

    21082025

---
+++ Java/SelectionSort.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size of Array you want to sort");
+        int n = sc.nextInt();
+        System.out.print("Enter the Elements of Array: ");
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minValue = arr[i];
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j]<minValue) {
+                    minValue = arr[j];
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+
+    }
+}
